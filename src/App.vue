a9076068b61621fdd32561e156d7d70402852142
Siegfried Hamm <siegfried.hamm@gmail.com>
2024-02-25 20:30:46 +0100

    testing for mobile III

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 
-  <div class="">
+  <div>
   <!-- navigation -->
-    <header class="w-full">
+    <header class="">
       <h1
         class="text-3xl font-bold font-freckle p-5 text-yellow-100 bg-yellow-500"
       >
